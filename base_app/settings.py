51781c77219cf7968945f9275f0e4e42eb32de4c
david isaac <davidisaacsurvive@gmail.com>
2022-10-05 16:15:55 +0100

    requiremets.txt packages updated

--- base_app/settings.py
+++ base_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bx9t0tsvpn8hd4el)o43e)@c84u@*om@v_yl4j40=b_+z*!g#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 ]
 
 
-# AUTH_USER_MODEL = "base.User"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
