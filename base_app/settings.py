ae30f56b2d0aba25959666ce632c10ea31eb7e7d
david isaac <davidisaacsurvive@gmail.com>
2022-10-07 11:09:03 +0100

    docker image file added:

--- base_app/settings.py
+++ base_app/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bx9t0tsvpn8hd4el)o43e)@c84u@*om@v_yl4j40=b_+z*!g#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -36,8 +36,6 @@ INSTALLED_APPS = [
     'drf_spectacular_sidecar', 
     'drf_yasg',
     "corsheaders"
-
-    
 ]
 
 
@@ -149,9 +147,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR,'static/')
+# STATIC_URL = os.path.join(BASE_DIR,'static/')
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
