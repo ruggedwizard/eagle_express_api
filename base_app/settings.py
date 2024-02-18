f56036ef32fc5dcd7dbdc63fccee03c9241fd18c
david isaac <davidisaacsurvive@gmail.com>
2024-02-18 00:42:02 -0800

    db error

--- base_app/settings.py
+++ base_app/settings.py
@@ -106,14 +106,24 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'verceldb',
+        'USER':'default',
+        'PASSWORD':'hbtqBUf2lME1',
+        'HOST':'ep-autumn-math-75513108-pooler.us-east-1.postgres.vercel-storage.com',
+        'PORT':'5432'
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
